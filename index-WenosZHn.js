6982356c8eacd8d7654a33b195b4ae4914bba0e7
ManzDev <JoseRomanHernandez@gmail.com>
2024-01-12 12:07:05 +0000

    Updates

--- index-WenosZHn.js
+++ index-WenosZHn.js
@@ -462,7 +462,7 @@
               #fff6 115deg 140deg,
               transparent 215deg 360deg
             );
-          box-shadow: 0 -1px 1px #fff inset;
+          box-shadow: 0 -1px 1px #666 inset;
           position: absolute;
         }
 
